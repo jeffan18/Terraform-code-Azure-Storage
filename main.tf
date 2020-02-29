0adb5ea7099205263abf428a6adaa763b6f55ef7
jeffan <jeffan18@gmail.com>
2020-02-29 17:39:26 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 provider "azurerm" {
-  subscription_id = ""
-  tenant_id = ""
-  client_id = ""
-  client_secret = ""
+  version         = "1.44.0"
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  tenant_id       = "${var.tenant_id}"
 }
 
 terraform {
@@ -11,6 +12,6 @@ terraform {
     storage_account_name  = "forterraformbackend"
     container_name        = "tfstate"
     access_key = "qXRMCmbVZuqd5OJm6Wk39MuYvlhnHdmgDABoQXppavuAzp2nzbvEc9I0dbrsQ2xiZH/njX/rjv30jrA7jCvgJQ=="
-    key = "project.static.website.txt"
+    key = "project.static.website.action.test.txt"
   }
 }
