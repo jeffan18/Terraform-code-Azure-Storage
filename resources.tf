dee3c92b69765ae37115896915a147c983b98f56
jeffan <jeffan18@gmail.com>
2020-02-29 18:36:51 -0500

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -11,9 +11,5 @@ resource "azurerm_storage_account" "fanfan" {
   account_tier              = "Standard"
   account_replication_type  = "LRS"
   enable_https_traffic_only = true
-
-  provisioner "local-exec" {
-    command = "az storage blob service-properties update --account-name ${azurerm_storage_account.fanfan.name} --static-website  --index-document index.html --404-document 404.html"
-  }
 }
 
